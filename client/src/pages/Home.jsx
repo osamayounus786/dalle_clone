fd8244b67ea3aaa83c5b52681f9adb1e950e5455
Osama Younus <osamayounus541@gmail.com>
2023-08-22 15:20:20 +0500

    frontend structure is done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,18 +2,20 @@ import React, {useState, useEffect} from 'react';
 
 import {Card, FormField, Loader} from '../components';
 
-const RenderCards = ({ data, title }) => {
-            if(data?.lenght > 0){
-                    return data.map((post)=>{
-                            <Card key={post._id} {...post}/>
-                    })
-            }
+const RenderCards = ({data, title}) => {
+    if (data ?. lenght > 0) {
+        return data.map((post) => {
+            <Card key={
+                    post._id
+                }
+                {...post}/>
+        })
+    }
 
-            return(
-                <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>
-                        {title}
-                </h2>
-            )
+    return (
+        <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>
+            {title} </h2>
+    )
 }
 
 const Home = () => {
@@ -36,25 +38,40 @@ const Home = () => {
             </div>
 
             <div className='mt-16'>
-                    <FormField />
+                <FormField/>
             </div>
 
             <div className='mt-10'>
-                    {
-                        loading ? (
-                        <div className='flex justify-center items-center'> <Loader /> </div>
-                        ) : (<>
-                            {searchText &&
-                                <h2 className='font-medium text-[#666e75] tex-xl mb-3'>
-                                   Showing results for <span className='text-[#222328]'>{searchText}</span>
-                                </h2>
-                            }
-                            <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
-
-                            </div>
-                        </>)
+                {
+                loading ? (
+                    <div className='flex justify-center items-center'>
+                        <Loader/>
+                    </div>
+                ) : (
+                    <> {
+                        searchText && <h2 className='font-medium text-[#666e75] tex-xl mb-3'>
+                            Showing results for
+                            <span className='text-[#222328]'>
+                                {searchText}</span>
+                        </h2>
                     }
-            </div>
+                        <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+                            {
+                            searchText ? (
+                                <RenderCards data={
+                                        []
+                                    }
+                                    title="No search results found"/>
+                            ) : (
+                                <RenderCards data={
+                                        []
+                                    }
+                                    title="No Posts found"/>
+                            )
+                        } </div>
+                    </>
+                )
+            } </div>
         </section>
     )
 }
