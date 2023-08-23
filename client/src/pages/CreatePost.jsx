fef590efa527720093d414e27b71ffb655472ed1
Osama Younus <osamayounus541@gmail.com>
2023-08-23 19:37:25 +0500

    project work stopped due to paid api

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -13,8 +13,34 @@ const CreatePost = () => {
     const [loading, setLoading] = useState(false);
 
 
-    const generateImage = ()=>{
+    const generateImage = async ()=>{
+              // integrating data from backend api 
 
+              if(form.prompt){
+
+                try {
+                  setGeneratingImg(true);
+                  const response = await fetch('http://localhost:8080/api/v1/dalle',{
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ prompt: form.prompt}),
+                  })
+                  const data = await response.json()
+                  setForm({...form, photo: `data:image/jpeg;base64,${data.photo}`})
+                } 
+                
+                catch (error) {
+                  alert(error)
+                }
+                finally{
+                  setGeneratingImg(false);
+                }
+            }
+            else{
+              alert('Please Enter a prompt')
+            }
     }
 
     const handleSubmit = ()=> {
